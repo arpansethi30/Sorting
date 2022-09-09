b8d53783c17be374698d127f854b4d084ed5b5cc
arpansethi30 <92069600+arpansethi30@users.noreply.github.com>
2022-09-09 21:26:05 +0530

    hii

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -6,14 +6,13 @@ public class BubbleSort {
             for (int j = 0; j < n-i-1; j++)
                 if (arr[j] > arr[j+1])
                 {
-                    // swap temp and arr[i]
+
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
     }
  
-    /* Prints the array */
     void printArray(int arr[])
     {
         int n = arr.length;
@@ -21,8 +20,7 @@ public class BubbleSort {
             System.out.print(arr[i] + " ");
         System.out.println();
     }
- 
-    // Driver method to test above
+
     public static void main(String args[])
     {
         BubbleSort ob = new BubbleSort();
